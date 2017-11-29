236ed786420130dfb8710bea5ff205d39076b6e5
LuongTheDungMiraway <dunglt@miraway.vn>
2017-11-29 14:35:55 +0700

    Get First Location

--- Booking/Controllers/HomeController.m
+++ Booking/Controllers/HomeController.m
@@ -11,27 +11,62 @@
 @import STPopup;
 @import FontAwesomeKit;
 
+#import <CoreLocation/CoreLocation.h>
 #import "HomeController.h"
 #import "MakerInfoView.h"
 #import "ScheduceTimeController.h"
 
-@interface HomeController ()<GMSMapViewDelegate>
+@interface HomeController ()<GMSMapViewDelegate, CLLocationManagerDelegate>{
+    CLLocationManager *locationManager;
+@protected BOOL getLocation ;
+}
 
+@property CLLocation *myLocation;
 @end
 
 @implementation HomeController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.delegate = self;
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
     
-    [self loadView];
+    [locationManager startUpdatingLocation];
+    
+}
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
+{
+    NSLog(@"didFailWithError: %@", error);
+    UIAlertView *errorAlert = [[UIAlertView alloc]
+                               initWithTitle:@"Error" message:@"Failed to Get Your Location" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [errorAlert show];
 }
 
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
+{
+    NSLog(@"didUpdateToLocation: %@", newLocation);
+    _myLocation = newLocation;
+    if (_myLocation.coordinate.longitude == newLocation.coordinate.longitude && _myLocation.coordinate.latitude == newLocation.coordinate.latitude)
+    {
+        if (getLocation)
+        {
+            [self loadView];
+        }
+        getLocation = true ;
+    }
+    else
+    {
+        [self loadView];
+    }
+    
+    
+}
 - (void)loadView {
     // Create a GMSCameraPosition that tells the map to display the
     // coordinate -33.86,151.20 at zoom level 6.
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
-                                                            longitude:151.20
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:_myLocation.coordinate.latitude
+                                                            longitude:_myLocation.coordinate.longitude
                                                                  zoom:6];
     GMSMapView *mapView = [GMSMapView mapWithFrame:CGRectZero camera:camera];
     mapView.myLocationEnabled = YES;
@@ -40,7 +75,7 @@
     
     // Creates a marker in the center of the map.
     NSArray* arrMarkerData = @[
-                               @{@"title": @"Sydney", @"snippet": @"Australia", @"position": [[CLLocation alloc]initWithLatitude:-33.86 longitude:151.20]},
+                               @{@"title": @"Sydney", @"snippet": @"Australia", @"position": [[CLLocation alloc]initWithLatitude:_myLocation.coordinate.latitude+0.02 longitude:_myLocation.coordinate.longitude+0.05]},
                                @{@"title": @"Other location", @"snippet": @"other snippet", @"position": [[CLLocation alloc]initWithLatitude:17.398932 longitude:78.472718]}
                                ];
     FAKIonIcons *icon = [FAKIonIcons iosLocationIconWithSize:40];
@@ -64,7 +99,7 @@
 
 - (UIView *)mapView:(GMSMapView *)mapView markerInfoWindow:(GMSMarker *)marker {
     MakerInfoView *view = [[MakerInfoView alloc] initWithFrame:CGRectMake(0, 0, 200, 140)];
-    [view setName:marker.title address:@"92a Yet Kieu Stress" peopleCount:2 timeWait:10];
+    [view setName:marker.title address:@"14 Pháo Đài Láng" peopleCount:2 timeWait:10];
     CGSize size = [view systemLayoutSizeFittingSize:CGSizeMake(220, 220) withHorizontalFittingPriority:UILayoutPriorityRequired verticalFittingPriority:UILayoutPriorityFittingSizeLevel];
     CGRect frame = CGRectMake(0, 0, size.width, size.height);
     view.frame = frame;
@@ -79,13 +114,14 @@
 
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
+
