deda4094f8307c9292b824e354b6801992962380
Luong The Dung <dunglt@miraway.vn>
2018-03-07 17:30:29 +0700

    chore - is wrapper UI for GMS Marker

---
+++ Booking/Views/MapMaker.h
@@ -0,0 +1,13 @@
+//
+//  MapMaker.h
+//  Booking
+//
+//  Created by LuongTheDung on 3/7/18.
+//  Copyright © 2018 Cau Ca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapMaker : UIView
+
+@end
---
+++ Booking/Views/MapMaker.m
@@ -0,0 +1,21 @@
+//
+//  MapMaker.m
+//  Booking
+//
+//  Created by LuongTheDung on 3/7/18.
+//  Copyright © 2018 Cau Ca. All rights reserved.
+//
+
+#import "MapMaker.h"
+
+@implementation MapMaker
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
