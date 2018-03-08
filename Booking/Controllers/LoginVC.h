3b2a70f4b681d90b6d22ad14052dc14ede997fc4
Luong The Dung <dunglt@miraway.vn>
2018-03-08 14:07:28 +0700

    add UI Login - waiting for icon design for finhish

---
+++ Booking/Controllers/LoginVC.h
@@ -0,0 +1,13 @@
+//
+//  LoginVC.h
+//  Booking
+//
+//  Created by LuongTheDung on 3/8/18.
+//  Copyright © 2018 Cau Ca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginVC : UIViewController
+
+@end
